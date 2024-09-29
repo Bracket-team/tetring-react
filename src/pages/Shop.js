d0851c81a90439789d88e697c62ed96108379935
lright-psw <52391568+lright-psw@users.noreply.github.com>
2024-09-29 16:33:15 +0900

    Update Shop.js

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // React Router의 useNavigate import
 import '../css/shop.css'; // CSS 파일
 import { useShopLogic } from '../game/shopLogic'; // 로직 파일 import
@@ -6,7 +6,15 @@ import { useShopLogic } from '../game/shopLogic'; // 로직 파일 import
 const Shop = ({round, setRound, gold, setGold, moneylevel, setMoneyLevel}) => {
   const { relic3, handleRelicBuy } = useShopLogic({ gold, setGold }); // 상점 로직 사용
   const navigate = useNavigate(); // navigate 함수 사용
+  
+  const [showModal, setShowModal] = useState(false); // 모달 표시 여부 관리
 
+  const handleModal = () => {
+    setShowModal(true); // 모달을 보여줌
+      setTimeout(() => {
+        setShowModal(false); // 1초 후에 모달을 숨김
+      }, 1000);
+  }
   const handleStartClick = () => {
     setRound(round + 1);
     navigate('/startgame');
@@ -16,16 +24,15 @@ const Shop = ({round, setRound, gold, setGold, moneylevel, setMoneyLevel}) => {
   const handleMoneylevelClick = () => {
     if (gold >= 10) {
       setGold(gold - 10);
-      setMoneyLevel(moneylevel + 1)
-    }
-    else {
-      console.log("돈부족");
+      setMoneyLevel(moneylevel + 1);
+    } else {
+      handleModal();
     }
   };
 
   // 유물 리롤
   const handleRerollClick = () => {
-    
+    // 리롤 기능 추가 예정
   };
 
   return (
@@ -36,6 +43,16 @@ const Shop = ({round, setRound, gold, setGold, moneylevel, setMoneyLevel}) => {
         <div id="targetScore">2400</div>
         <div id="gold">보유 금액 : {gold}$</div>
       </div>
+
+      {/* 모달창 */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span>돈이 부족합니다</span>
+          </div>
+        </div>
+      )}
+
       <div className="background">
         <div className="relic_box">
           <div id="relic1"><img src="../img/block1.png" className="relic" alt="relic 1" /></div>
